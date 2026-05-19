3f777fccb94004eddb486eb2a3cc8f1205c655a9
Ensrick <truthisfact@gmail.com>
2026-05-19 12:49:22 -0500

    Release v0.3.3

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace WorkshopSentinel;
 
 public static class Program
 {
-    public const string Version = "0.3.2";
+    public const string Version = "0.3.3";
 
     [STAThread]
     public static int Main(string[] args)
